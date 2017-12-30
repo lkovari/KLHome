d3c76428693868b68501b268aab29171783c5447
LK <laszlo.kovary@gmail.com>
2017-12-30 20:39:47 +0100

    Added SASS Udemz certificate

--- src/app/layout/angular-page/angular-page-content1/angular-page-content1.component.ts
+++ src/app/layout/angular-page/angular-page-content1/angular-page-content1.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./angular-page-content1.component.scss']
 })
 export class AngularPageContent1Component implements OnInit {
-  fullImagePath: string;
+  fullImagePathSCSS: string;
+  fullImagePathSASS: string;
   constructor() { 
-    this.fullImagePath = 'assets/images/UC-XRLSVCSR.jpg';
+    this.fullImagePathSCSS = 'assets/images/UC-XRLSVCSR.jpg';
+    this.fullImagePathSASS = 'assets/images/UC-NLUM4IQ0.jpg';
   }
 
   ngOnInit() {
