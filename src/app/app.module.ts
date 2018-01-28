7646d37603a7b0d86973150e54ec9855430bf142
LK <laszlo.kovary@gmail.com>
2018-01-29 00:33:03 +0100

    -added back providers in app.module.ts prevent 404 error when refrest (hashbang) -centered scheduling carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,9 +20,9 @@ import { SharedModule } from 'app/shared/shared.module';
         AppRoutingModule,
         SharedModule
     ],
-  // prevent 404 Forbidden error when refresh the page
-  // providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
-  providers: [],
+  // prevent 404 Forbidden error when refresh the page 
+  // https://stackoverflow.com/questions/35284988/angular-2-404-error-occur-when-i-refresh-through-the-browser
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
