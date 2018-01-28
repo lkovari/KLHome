03fab7873a3de4b4109cd7430d4265d892dc6a87
LK <laszlo.kovary@gmail.com>
2018-01-28 20:46:08 +0100

    -Added full path to carousel slider (I hope it will works) -Added scss for Android AE.mobile page

--- src/app/layout/j2se-page/j2se-page-content1/j2se-page-content1.component.ts
+++ src/app/layout/j2se-page/j2se-page-content1/j2se-page-content1.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./j2se-page-content1.component.scss']
 })
 export class J2sePageContent1Component implements OnInit {
-  public imagePathArray = ['assets/images/dev-j2se-dtpicket.jpg', 'assets/images/dev-j2se-dpicker.jpg', 'assets/images/dev-j2se-tpicker.jpg'];
+  public imagePathArray = ['https://lkovari.github.io/KLHome/assets/images/dev-j2se-dtpicket.jpg',
+        'https://lkovari.github.io/KLHome/assets/images/dev-j2se-dpicker.jpg',
+        'https://lkovari.github.io/KLHome/assets/images/dev-j2se-tpicker.jpg'
+      ];
 
   constructor() { }
 
