91b97dafa2561beba616c275dd85fe3c75f3a32f
LK <laszlo.kovary@gmail.com>
2018-03-07 23:27:51 +0100

    -modify date diff functions

--- src/app/shared/modules/page-header/page-header.component.ts
+++ src/app/shared/modules/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -23,59 +23,60 @@ export class PageHeaderComponent implements OnInit {
     this.duration = this.dateDiff(this.startDate, this.endDate);
   }
 
+  private dateDiffInDays(date1: Date, date2: Date): number {
+    const oneday = 1000 * 60 * 60 * 24;
+    const date1InMs = date1.getTime();
+    const date2InMs = date2.getTime();
+    const differenceInMs = date2InMs - date1InMs;
+    return Math.round(differenceInMs / oneday);
+  }
+
+  private dateDiffInText(diffInDays: number): string {
+    let diffText = '';
+    const years = Math.trunc(diffInDays / 365);
+    const remaindedMonths = diffInDays - (years * 365);
+    const months = Math.trunc(remaindedMonths / 30);
+    if (years === 0) {
+      if (months > 0) {
+        if (months > 1) {
+          diffText = '' + months + ' months';
+        } else {
+          diffText = '' + months + ' month';
+        }
+      } else {
+        diffText = '';
+      }
+    } else {
+      if (years > 1) {
+        diffText = '' + years + ' years ';
+        if (months > 0) {
+          if (months > 1) {
+            diffText = diffText + months + ' months';
+          } else {
+            diffText = diffText + months + ' month';
+          }
+        } else {
+          diffText += '';
+        }
+      } else {
+        diffText = '' + years + ' year ';
+        if (months > 0) {
+          if (months > 1) {
+            diffText = diffText + months + ' months';
+          } else {
+            diffText = diffText + months + ' month';
+          }
+        } else {
+          diffText += '';
+        }
+      }
+    }
+    return diffText;
+  }
+
   private dateDiff(date1: Date, date2: Date): string {
     let res = '';
-    let years = 0;
-    let months = 0;
-
-    const month1 = date1.getMonth() + 1;
-    const month2 = date2.getMonth() + 1;
-    const fullYear1 = date1.getFullYear();
-    const fullYear2 = date2.getFullYear();
-    let monthsAdd = 0;
-    if (month1 === month2) {
-      months = (fullYear2 - fullYear1) * 12;
-    } else {
-      if (fullYear1 === fullYear2) {
-        if (month1 < month2) {
-          monthsAdd = (month2 - month1);
-        } else if (month1 > month2) {
-          monthsAdd = ((12 - month1) + month2);
-        }
-      } else {
-        months = ((fullYear2 - fullYear1)) * 12;
-        if (month1 < month2) {
-          monthsAdd = (month2 - month1);
-        } else if (month1 > month2) {
-          if (month1 === 12) {
-            months = months - month1;
-          }
-          monthsAdd = ((12 - month1) + month2);
-        }
-      }
-    }
-    months += monthsAdd;
-    months = months <= 0 ? 0 : months;
-    if (months >= 12) {
-      if ((months % 12) === 0) {
-        years = months / 12;
-        months = 0;
-      } else {
-        const monthsRemainder = months % 12;
-        years = (months - (monthsRemainder % 12)) / 12;
-        months = monthsRemainder;
-      }
-    }
-    if (years === 0) {
-      res = '' + months + ' month' + (months > 1 ? 's' : '');
-
-    } else {
-      if (months === 0) {
-        res = '' + years + ' year' + (years > 1 ? 's' : '');
-      } else {
-        res = '' + years + ' year' + (years > 1 ? 's ' : ' ') + months + ' month' + (months > 1 ? 's' : '');
-      }
-    }
+    res = this.dateDiffInText(this.dateDiffInDays(date1, date2));
     return res;
   }
 }
