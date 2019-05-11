ddebb429980ac164ae2caaabf7b8a9675e1893c4
LK <laszlo.kovary@gmail.com>
2019-05-12 00:07:09 +0200

    Updates

--- app/layout/angular-page/angular-page-content1/angular-page-content1.component.ts
+++ app/layout/angular-page/angular-page-content1/angular-page-content1.component.ts
@@ -31,7 +31,7 @@ export class AngularPageContent1Component implements OnInit {
   fullImagePathCSSPos: string;
   fullImageRxJs: string;
   angularVersion: any;
-  courseTitles = [ 'Title', 'Author', 'Site', 'Completed', 'Certificate' ];
+  courseTitles = ['Title', 'Author', 'Site', 'Completed', 'Certificate'];
   totalComplettedHours: string;
 
   constructor(private fileLoaderService: FileLoaderService) { }
@@ -50,40 +50,36 @@ export class AngularPageContent1Component implements OnInit {
         const csvHeader = textLines[0].split(',');
         console.log('csvHeader ' + fileName + ' - ' + JSON.stringify(csvHeader));
         let isCompleted = false;
-        try {
-          for (let ix = 1; ix < textLines.length; ix++) {
-            // split content based on comma
-            const data = textLines[ix].split(',');
-            console.log('length ' + fileName + ' - ' + JSON.stringify(data[0]));
-            if (data.length === 7 && data.length === csvHeader.length) {
-              data[0] = data[0].replace('↵', '');
-              console.log('data ' + fileName + ' - ' + JSON.stringify(data[0]));
-              const courseModel = new AngularCourseModel();
-              courseModel.title = data[0];
-              courseModel.author = data[1];
-              courseModel.website = data[2];
-              courseModel.dateOfCompleted = (data[3] !== 'null') ? new Date(data[3]) : null;
-              if (courseModel.dateOfCompleted) {
-                isCompleted = true;
-              }
-              courseModel.certificateImageLarge = (data[4] !== 'null') ? 'assets/images/' + data[4] : null;
-              courseModel.certificateImageSmall = (data[4] !== 'null') ? 'assets/images/' + data[4] : null;
-              courseModel.hours = (data[5] !== '0') ? +data[5] : 0;
-              courseModel.minutes = (data[6] !== '0') ? +data[6] : 0;
-              courses.push(courseModel);
-            } else {
-              console.log('Length not equals ' + fileName + ' d ' + data.length + ' h ' + csvHeader.length + ' : ' + data[0]);
+        for (let ix = 1; ix < textLines.length; ix++) {
+          // split content based on comma
+          const data = textLines[ix].split(',');
+          console.log('length ' + fileName + ' - ' + JSON.stringify(data[0]));
+          if (data.length === 7 && data.length === csvHeader.length) {
+            data[0] = data[0].replace('↵', '');
+            console.log('data ' + fileName + ' - ' + JSON.stringify(data[0]));
+            const courseModel = new AngularCourseModel();
+            courseModel.title = data[0];
+            courseModel.author = data[1];
+            courseModel.website = data[2];
+            courseModel.dateOfCompleted = (data[3] !== 'null') ? new Date(data[3]) : null;
+            if (courseModel.dateOfCompleted) {
+              isCompleted = true;
             }
+            courseModel.certificateImageLarge = (data[4] !== 'null') ? 'assets/images/' + data[4] : null;
+            courseModel.certificateImageSmall = (data[4] !== 'null') ? 'assets/images/' + data[4] : null;
+            courseModel.hours = (data[5] !== '0') ? +data[5] : 0;
+            courseModel.minutes = (data[6] !== '0') ? +data[6] : 0;
+            courses.push(courseModel);
+          } else {
+            console.log('Length not equals ' + fileName + ' d ' + data.length + ' h ' + csvHeader.length + ' : ' + data[0]);
           }
-          console.log('PARSED ' + fileName + ' - ' + courses.length  + ' : ' + JSON.stringify(courses));
-          if (isCompleted) {
-            this.angularCourseCompletedList.sort((course1: AngularCourseModel, course2: AngularCourseModel) => {
-              return course2.dateOfCompleted.getTime() - course1.dateOfCompleted.getTime();
-            });
-            this.calculateTotalHours();
-          }
-        } catch (e) {
-          console.log('ERROR ' + e);
+        }
+        console.log('PARSED ' + fileName + ' - ' + courses.length + ' : ' + JSON.stringify(courses));
+        if (isCompleted) {
+          this.angularCourseCompletedList.sort((course1: AngularCourseModel, course2: AngularCourseModel) => {
+            return course2.dateOfCompleted.getTime() - course1.dateOfCompleted.getTime();
+          });
+          this.calculateTotalHours();
         }
       }
     });
@@ -93,10 +89,10 @@ export class AngularPageContent1Component implements OnInit {
   initializeAngularCourses() {
     this.angularCourseCompletedList = [];
     this.csvLoaderParser('assets/courses', 'completed-courses.csv', this.angularCourseCompletedList);
-    this.angularCourseInProgressList =  [];
+    this.angularCourseInProgressList = [];
     this.csvLoaderParser('assets/courses', 'inprogress-courses.csv', this.angularCourseInProgressList);
 
-    this.angularCoursePlannedList =  []
+    this.angularCoursePlannedList = []
     this.csvLoaderParser('assets/courses', 'planned-courses.csv', this.angularCoursePlannedList);
   }
 
