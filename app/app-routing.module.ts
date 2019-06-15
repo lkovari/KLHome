436d0621345b853a35874a7d0c08a9ce2f7e444e
LK <laszlo.kovary@gmail.com>
2019-06-15 12:01:58 +0200

    Updates

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true, useHash: true })],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false, useHash: true })],
   exports: [RouterModule],
   declarations: []
 })
