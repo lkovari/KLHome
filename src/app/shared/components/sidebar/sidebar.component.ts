5381ddd171365b8afcd53da5bd6a7071771843e6
LK <laszlo.kovary@gmail.com>
2019-07-16 20:43:07 +0200

    changes last update

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class SidebarComponent {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('07/16/2019 08:41 PM');
+  lastUpdate = new Date('07/16/2019 08:42 PM');
   lastUpdateTooltip = 'Added reactive example of basic form';
 
   eventCalled() {
