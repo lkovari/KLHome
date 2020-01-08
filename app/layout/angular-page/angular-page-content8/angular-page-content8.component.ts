fd00cf161ad9187485430bcbdd6a6bd4656b2f7c
LK <laszlo.kovary@gmail.com>
2020-01-08 11:32:37 +0100

    Updates

--- app/layout/angular-page/angular-page-content8/angular-page-content8.component.ts
+++ app/layout/angular-page/angular-page-content8/angular-page-content8.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Person } from 'app/shared/models/person.model';
 import { PerfectScrollbarConfigInterface, PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
+import { ExamplePersonDataService } from 'app/shared/services/examplepersondata/example-person-data.service';
 
 @Component({
   selector: 'app-angular-page-content8',
@@ -13,14 +14,36 @@ export class AngularPageContent8Component implements OnInit {
   personList: Array<Person>;
   config: PerfectScrollbarConfigInterface = {};
   @ViewChild(PerfectScrollbarComponent, { static: false }) componentRef?: PerfectScrollbarComponent;
+  useRouteResolver: boolean;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private examplePersonDataService: ExamplePersonDataService) { }
 
   ngOnInit() {
+    this.useRouteResolver = this.examplePersonDataService.useRouteResolver;
     this.personList = [];
     this.githubLogoPath = 'assets/githubmark/GitHub-Mark-32px.png';
-    this.personList = this.route.snapshot.data['resolvedPersonData'];
-    console.log(`AngularPageContent8Component : # of Person row loaded ${this.personList.length}`);
+    if (this.useRouteResolver) {
+      this.personList = this.route.snapshot.data['resolvedPersonData'];
+      console.log(`RESOLVE: AngularPageContent8Component : #${this.personList.length} Person rows captured from route data.`);
+      this.examplePersonDataService.useRouteResolver = false;
+    } else {
+      this.examplePersonDataService.loadtExamplePersonData().subscribe((personArray: Array<Person>) => {
+        if (personArray) {
+          Object.assign(this.personList, personArray);
+          console.log(`LOAD: AngularPageContent8Component : #${this.personList.length} Person rows captured from route data.`);
+        }
+        this.examplePersonDataService.useRouteResolver = true;
+      });
+    }
+    try {
+      console.log(`RESOLVED:
+      Id:${this.personList[9999].id},
+      First name: ${this.personList[9999].firstName},
+      Last Name: ${this.personList[9999].lastName}`);
+    } catch(e) {
+      window.alert(`ERROR!\n ${e.stack}`);
+      throw e;
+    }
   }
 
   onScrolltoTopClicked(event): void {
