d74fb391e5fdd7083967b62921be4b022ddee501
LK <laszlo.kovary@gmail.com>
2020-04-11 12:36:37 +0200

    upgraded to Angular v9.1.1 PrimeNG 9.0.5, TypeScript v3.8.3 etc.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('04/10/2020 04:27 PM');
-  lastUpdateTooltip = 'Set strictNullChecks in the tsconfig.json';
+  lastUpdate = new Date('04/11/2020 01:00 PM');
+  lastUpdateTooltip = 'Set strictNullChecks in the tsconfig.json upgraded to Angular v9.1.1';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
