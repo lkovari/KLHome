5e08ad3544c4117865808ba785e4b575c3c3bc0e
LK <laszlo.kovary@gmail.com>
2020-04-11 12:57:45 +0200

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('04/11/2020 01:00 PM');
+  lastUpdate = new Date('04/11/2020 01:05 PM');
   lastUpdateTooltip = 'Set strictNullChecks in the tsconfig.json upgraded to Angular v9.1.1';
 
   ngOnInit() {
