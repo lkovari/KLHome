dc406cfe50e2c37b27b37e649a5909ae984d02eb
LK <laszlo.kovary@gmail.com>
2020-05-09 15:11:01 +0200

    fix date format problem

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('05/09/2020 03:079 PM');
+  lastUpdate = new Date('05/09/2020 03:07 PM');
   lastUpdateTooltip = 'Playground #9 Added orientation to BarChart component';
 
   ngOnInit() {
