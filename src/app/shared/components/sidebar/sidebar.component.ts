073cd3005e5a078e3d46bbff4c7257800862a1d6
LK <laszlo.kovary@gmail.com>
2020-09-06 20:58:50 +0200

    Fixed css error commit package lock json

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('09/06/2020 06:04 PM');
+  lastUpdate = new Date('09/06/2020 08:19 PM');
   lastUpdateTooltip = 'Upgrade to v10 the Angular Material, and added other completed courses';
 
   ngOnInit() {
