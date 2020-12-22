1fa056be1a161c749e6c49e16a4539219dcece30
LK <laszlo.kovary@gmail.com>
2020-12-22 20:50:16 +0100

    Continue to implement a reactive CheckListComponent, workaround to fix validation problem

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('12/21/2020 11:37 PM');
-  lastUpdateTooltip = 'Playground #11, continue to implement a reactive CheckListComponent, align checkmark';
+  lastUpdate = new Date('12/22/2020 03:48 PM');
+  lastUpdateTooltip = 'Continue to implement a reactive CheckListComponent, workaround to fix validation problem';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
