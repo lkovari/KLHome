edc262c9e56517a6016881125877108f3f534b8c
LK <laszlo.kovary@gmail.com>
2021-01-23 19:59:54 +0100

    fix date format

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('12/25/2020 13:36 PM');
+  lastUpdate = new Date('12/25/2020 01:36 PM');
   lastUpdateTooltip = 'Continue to implement a reactive CheckListComponent, align checkmark icon';
 
   ngOnInit() {
