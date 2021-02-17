7a04fa740a435368cfef6f227fa5b3b52e47251c
LK <laszlo.kovary@gmail.com>
2021-02-17 21:29:25 +0100

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('01/24/2021 05:33 PM');
+  lastUpdate = new Date('02/17/2021 09:05 PM');
   lastUpdateTooltip = 'Update to Angular 11, PrimeNG 11 etc.';
 
   ngOnInit() {
