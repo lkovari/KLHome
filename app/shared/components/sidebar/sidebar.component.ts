5751718921d7d61b5012132883884d8387da022e
LK <laszlo.kovary@gmail.com>
2021-02-25 23:07:47 +0100

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('02/25/2021 10:08 PM');
+  lastUpdate = new Date('02/25/2021 11:04 PM');
   lastUpdateTooltip = 'Added pure pipe IsChecklisItemSelectedPipe';
 
   ngOnInit() {
