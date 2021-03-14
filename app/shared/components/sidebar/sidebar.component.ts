1004580f45200ae6505dbd29e70d739062c37d4a
LK <laszlo.kovary@gmail.com>
2021-03-14 17:51:15 +0100

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('03/07/2021 01:51 PM');
-  lastUpdateTooltip = 'Installed lk-form-validation monitor npm, and using it.';
+  lastUpdate = new Date('03/14/2021 05:46 PM');
+  lastUpdateTooltip = 'Updated lk-form-validation monitor npm, and using it.';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
