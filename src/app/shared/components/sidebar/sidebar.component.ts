75acb199cf2f2a2dd973336d8c1abc7a630409d5
LK <laszlo.kovary@gmail.com>
2021-03-16 20:21:37 +0100

    Updated lk-form-validation monitor npm, and using it. Turn layouts to more responsive

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('03/14/2021 05:46 PM');
-  lastUpdateTooltip = 'Updated lk-form-validation monitor npm, and using it.';
+  lastUpdate = new Date('03/16/2021 08:19 PM');
+  lastUpdateTooltip = 'Updated lk-form-validation monitor npm, and using it. Turn layouts to more responsive';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
