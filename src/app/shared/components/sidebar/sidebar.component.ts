be4d43d6eb2cc739e0195e239b361910cfcce17d
LK <laszlo.kovary@gmail.com>
2021-09-22 23:11:16 +0200

    fix mispelled words, add $ to variable name

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('08/26/2021 10:08 PM');
-  lastUpdateTooltip = 'Added simulated save notification at Playground#12.';
+  lastUpdate = new Date('09/21/2021 10:14 PM');
+  lastUpdateTooltip = 'Mispelled words correction, and variable name of the unsubscribe on Playground#12.';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
