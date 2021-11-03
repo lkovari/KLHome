5fd919956a981b90da69392927ab6f574f3b8d01
LK <laszlo.kovary@gmail.com>
2021-11-03 22:53:29 +0100

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('11/03/2021 21:41 PM');
+  lastUpdate = new Date('11/03/2021 09:41 PM');
   lastUpdateTooltip = 'marks Zip on Tab2 of page6 required, replaced FormControlwith AbstractControl on page12';
 
   ngOnInit() {
