4af29d16acdd03b8fe671d28cd0b816a9b262536
LK <laszlo.kovary@gmail.com>
2021-11-09 00:15:18 +0100

    Updates

--- app/layout/angular-page/angular-page-content6/zip-code-validator.ts.ts
+++ app/layout/angular-page/angular-page-content6/zip-code-validator.ts.ts
@@ -1,5 +1,6 @@
-import { AbstractControl, AsyncValidatorFn, ValidationErrors } from "@angular/forms";
-import { Observable } from "rxjs";
+import { Injectable, Injector } from "@angular/core";
+import { AbstractControl, AsyncValidator, /*AsyncValidatorFn,*/ ValidationErrors } from "@angular/forms";
+import { Observable, } from "rxjs";
 import { debounceTime, map } from "rxjs/operators";
 import { ZipCodeService } from "./services/zip-code.service";
 
@@ -7,32 +8,34 @@ import { ZipCodeService } from "./services/zip-code.service";
  * 
  * @param zipCodeService: ZipCodeService
  * @returns AsyncValidatorFn
- */
+ *
 export function ZipCodeValidator(zipCodeService: ZipCodeService): AsyncValidatorFn {
     return (ctrl: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
         return zipCodeService.zipCodeExists(parseInt(ctrl.value), false).pipe(
             debounceTime(1500),
             map((exists: boolean) => {
-                const res = (exists) ? null : { invalidZipCodeAsync: true };
+                const res = (exists) ? null : { zipCodeNotFoundAsync: true };
                 // console.log(`zipCodeValidator zip ${ctrl.value} errors '${this.customForm.get('customTab2')?.get('zipCode')?.errors} zip exists ${exists}.`);
                 return res;
             }
         ));
     };
 }
+*/
 
-/*
 @Injectable({
     providedIn: 'root'
 })
 export class ZipCodeValidator implements AsyncValidator {
 
-    constructor(private zipCodeService: ZipCodeService) {}
+    constructor(private injector: Injector) {}
 
     validate(c: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-        return this.zipCodeService.zipCodeExists((<FormControl>c).value).pipe(
-            map(exists => exists ? null : { invalidZipCodeAsync: true})
+        var zipCodeService = this.injector.get(ZipCodeService);
+        return zipCodeService.zipCodeExists(parseInt(c.value), false).pipe(
+            debounceTime(1500),
+            map((exists: boolean) => exists ? null : { zipCodeNotFoundAsync: true} )
         );
     }
 }
-*/
+
