266c8a2de17f32f231a0de3db75472b3a75d4014
LK <laszlo.kovary@gmail.com>
2021-11-09 21:55:42 +0100

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('11/09/2021 00:12 AM');
+  lastUpdate = new Date('11/09/2021 09:49 PM');
   lastUpdateTooltip = 'async validator variants on page #6 tab #3';
 
   ngOnInit() {
