e4c1113b993f4a7cecd0706f33edbb6e11af5717
LK <laszlo.kovary@gmail.com>
2021-12-19 00:02:26 +0100

    Modified J2SE pages AnalogClock & RPN Calc, replaced the image and added Github refs.

--- src/app/layout/j2se-page/j2se-page-content3/j2se-page-content3.component.ts
+++ src/app/layout/j2se-page/j2se-page-content3/j2se-page-content3.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./j2se-page-content3.component.scss']
 })
 export class J2sePageContent3Component implements OnInit {
-
+  githubLogoPath;
   constructor() { }
 
   ngOnInit() {
+    this.githubLogoPath = 'assets/githubmark/GitHub-Mark-32px.png';
   }
 
 }
