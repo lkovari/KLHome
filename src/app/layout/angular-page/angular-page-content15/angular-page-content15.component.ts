373abb7e71ba9cb4f267ddd5ee747f5bf8a05a27
LK <laszlo.kovary@gmail.com>
2022-06-01 22:55:51 +0200

    switchMap solution

--- src/app/layout/angular-page/angular-page-content15/angular-page-content15.component.ts
+++ src/app/layout/angular-page/angular-page-content15/angular-page-content15.component.ts
@@ -1,7 +1,9 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable, of, Subject } from 'rxjs';
+import { debounceTime, switchMap } from 'rxjs/operators';
+import { OperationModeKind } from './operation-mode-kind.enum';
 
 @Component({
   selector: 'app-angular-page-content15',
@@ -9,6 +11,8 @@ import { debounceTime } from 'rxjs/operators';
   styleUrls: ['./angular-page-content15.component.scss']
 })
 export class AngularPageContent15Component implements OnInit {
+  INCREMENT_ONE = 1;
+  DECREMENT_ONE = -1;
   githubLogoPath: string;
   defaultRows = 5;
   logValue: string;
@@ -18,56 +22,84 @@ export class AngularPageContent15Component implements OnInit {
   maxRowstLimit = 10;  
   debounceTimeValue = 500;
   displayDate: Date;
-  withDebounceTime = false;
-  useDebounceTime: boolean;
+  useSelectedOperator = false;
+  operationModeKind = OperationModeKind.KIND_DEBOUNCE;
+  operationModeKindIsDebounce: boolean;
+  lastOperationModeKindIsDebounce: boolean;
   datePipe = new DatePipe('en-US');
   private buttonClickedSubject = new Subject<number>();
-
+  buttonClicked$: Observable<number>;
+  @ViewChild('dataEntryForm', {static: true} ) dataEntryForm: NgForm;
+  
   constructor() { }
 
   ngOnInit(): void {
     this.githubLogoPath = 'assets/githubmark/GitHub-Mark-32px.png';
     this.logValue = '';
     this.displayDate = new Date(Date.now());
-    this.useDebounceTime = true;
+    this.operationModeKindIsDebounce = true;
+    this.lastOperationModeKindIsDebounce = true;
+    this.evaluateModeKind();
+    this.setupObservable();
 
-    const buttonClickedDebounced = this.buttonClickedSubject.pipe(debounceTime(this.debounceTimeValue));
-    buttonClickedDebounced.subscribe((value: number) => {
+    this.buttonClicked$.subscribe((value: number) => {
       console.log('Value ' + value);
       const dt = this.datePipe.transform(this.displayDate, 'MMM d, y');
       this.logValue = this.logValue + ' ' + dt + '\n';
     });
-
+    this.dataEntryForm.statusChanges?.subscribe(() => {
+      this.evaluateModeKind();
+    });
     
   }
 
   private setNewDate(date: Date, increment: boolean) {
     let newDate = 0;
     if (increment) {
-      newDate = date.setDate(date.getDate() + 1);
+      newDate = date.setDate(date.getDate() + this.INCREMENT_ONE);
     } else {
-      newDate = date.setDate(date.getDate() + -1);
+      newDate = date.setDate(date.getDate() + this.DECREMENT_ONE);
     }
     this.displayDate = new Date(newDate);
   }
 
   onClickLeft() {
     this.setNewDate(this.displayDate, false);
-    if (this.withDebounceTime) {
-      this.buttonClickedSubject.next(-1);
+    if (this.useSelectedOperator) {
+      switch (this.operationModeKind) {
+        case OperationModeKind.KIND_DEBOUNCE : {
+          this.buttonClickedSubject.next(this.DECREMENT_ONE);
+          break;
+        }
+        case OperationModeKind.KIND_SWITCHMAP : {
+          this.buttonClickedSubject.next(this.DECREMENT_ONE);
+          break;
+        }
+      }
     } else {
       const dt = this.datePipe.transform(this.displayDate, 'MMM d, y');
       this.logValue = this.logValue + ' ' + dt + '\n';
+      console.log('onClickRight value:' + this.DECREMENT_ONE);
     }
   }
 
   onClickRight() {
     this.setNewDate(this.displayDate, true);
-    if (this.withDebounceTime) {
-      this.buttonClickedSubject.next(1);
+    if (this.useSelectedOperator) {
+      switch (this.operationModeKind) {
+        case OperationModeKind.KIND_DEBOUNCE : {
+          this.buttonClickedSubject.next(this.INCREMENT_ONE);
+          break;
+        }
+        case OperationModeKind.KIND_SWITCHMAP : {
+          this.buttonClickedSubject.next(this.INCREMENT_ONE);
+          break;
+        }
+      }
     } else {
       const dt = this.datePipe.transform(this.displayDate, 'MMM d, y');
       this.logValue = this.logValue + ' ' + dt + '\n';
+      console.log('onClickRight value:' + this.INCREMENT_ONE);
     }
   }
 
@@ -86,5 +118,30 @@ export class AngularPageContent15Component implements OnInit {
 
   onDateSelected(date: Date) {
     console.log('onDateSelected event arg ' + JSON.stringify(date) + ' customDate ' + JSON.stringify(this.displayDate));
+  }
+
+  setupObservable() {
+    switch (this.operationModeKind) {
+      case OperationModeKind.KIND_DEBOUNCE : {
+        this.buttonClicked$ = this.buttonClickedSubject.pipe(debounceTime(this.debounceTimeValue));
+        break;
+      }
+      case OperationModeKind.KIND_SWITCHMAP: {
+        this.buttonClicked$ = this.buttonClickedSubject.pipe(switchMap(
+          inputData => {
+            return of(inputData);
+          }
+        ));
+        break;
+      }
+    }    
+  }
+
+  evaluateModeKind() {
+    if (this.lastOperationModeKindIsDebounce !== this.operationModeKindIsDebounce) {
+      this.lastOperationModeKindIsDebounce = this.operationModeKindIsDebounce;
+      this.operationModeKind = this.operationModeKindIsDebounce ?  OperationModeKind.KIND_DEBOUNCE : OperationModeKind.KIND_SWITCHMAP;
+      this.setupObservable();
+    }
   }
 }
---
+++ src/app/layout/angular-page/angular-page-content15/operation-mode-kind.enum.ts
@@ -0,0 +1,6 @@
+
+
+export enum OperationModeKind {
+    KIND_DEBOUNCE,
+    KIND_SWITCHMAP
+}
