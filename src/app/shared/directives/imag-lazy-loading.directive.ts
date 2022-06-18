43ca32fbbd426e0e5133be7661d27d5e6bd81df2
LK <laszlo.kovary@gmail.com>
2022-06-18 21:50:42 +0200

    logging when set loading="lazy" attribute

--- src/app/shared/directives/imag-lazy-loading.directive.ts
+++ src/app/shared/directives/imag-lazy-loading.directive.ts
@@ -1,21 +1,21 @@
 import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 @Directive(
-    { selector: 'img' }
+  { selector: 'img' } // the selection target is the img HTML element
 )
 export class ImageLazyLoadingDirective extends Directive implements AfterViewInit {
 
-    constructor(private elementRef: ElementRef<HTMLImageElement>) {
-        super();
+  constructor(private elementRef: ElementRef<HTMLImageElement>) {
+    super();
+  }
+
+  ngAfterViewInit(): void {
+    const img = this.elementRef.nativeElement;
+    if ('loading' in HTMLImageElement.prototype) {
+      img.setAttribute('loading', 'lazy');
+      console.log('>>> Attribute loading="lazy" is set to img element src="' + img.src + '"');
+    } else {
+      console.log('>>> loading lazy is NOT supported!');
     }
-    
-    ngAfterViewInit(): void {
-        const img = this.elementRef.nativeElement;
-        if ('loading' in HTMLImageElement.prototype) {
-          img.setAttribute('loading', 'lazy');
-          console.log('>>> loading lazy is supported!');
-        } else {
-          console.log('>>> loading lazy is NOT supported!');
-        }
-    }
+  }
 }
