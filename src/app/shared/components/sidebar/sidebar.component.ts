e07b6a177c3ed5f90a4a61372e68e1b3f0f34dcd
LK <laszlo.kovary@gmail.com>
2022-06-19 01:39:40 +0200

    fix css

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('06/18/2022 10:36 PM');
+  lastUpdate = new Date('06/19/2022 01:3p AM');
   lastUpdateTooltip = 'fixed the SharedModule import & logging when set loading="lazy" attribute';
 
   ngOnInit() {
