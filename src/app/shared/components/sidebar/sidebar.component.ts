047961fd24eda82404b024195e409dec6101c22b
LK <laszlo.kovary@gmail.com>
2022-08-14 22:51:13 +0200

    Update to Angular 12, added Q2 2022 award

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('06/19/2022 01:05 PM');
-  lastUpdateTooltip = 'added :not(noLazy) excludes thowe img where noLazy exist (ImageLazyLoadingDirective)';
+  lastUpdate = new Date('08/14/2022 10:49 PM');
+  lastUpdateTooltip = 'Update to Angular 12, added Q2 2022 award';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
