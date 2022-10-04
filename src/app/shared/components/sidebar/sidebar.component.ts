8a411716a3149a1ed16094c615d0fc9070fcb411
LK <laszlo.kovary@gmail.com>
2022-10-04 23:54:36 +0200

    last update updated

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('08/20/2022 07:42 PM');
-  lastUpdateTooltip = 'modified tsconfig, strictNullChecks, strictTemplates .';
+  lastUpdate = new Date('09/04/2022 11:53 PM');
+  lastUpdateTooltip = 'cv updated.';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
