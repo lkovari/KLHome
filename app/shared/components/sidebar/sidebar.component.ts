c54a04cbfae572488e48ad7ad30c330415918a8d
LK <laszlo.kovary@gmail.com>
2022-11-07 14:25:22 +0100

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('11/07/2022 02:14 PM');
+  lastUpdate = new Date('11/07/2022 02:20 PM');
   lastUpdateTooltip = 'Added validation rules to page #10.';
 
   ngOnInit() {
