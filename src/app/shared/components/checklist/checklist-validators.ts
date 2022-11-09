e7f5b6133f089bbfee9ffb3825c14b403f7926ae
LK <laszlo.kovary@gmail.com>
2022-11-09 22:24:01 +0100

    checklist validation

--- src/app/shared/components/checklist/checklist-validators.ts
+++ src/app/shared/components/checklist/checklist-validators.ts
@@ -1,7 +1,7 @@
 import { FormControl, FormArray, ValidationErrors } from '@angular/forms';
 
 export class ChecklistValidators {
-
+    /*
     static mandatoryFieldValidator(c: FormControl): ValidationErrors | null {
         const labelControl = c.get('label');
         const idControl = c.get('id');
@@ -14,18 +14,18 @@ export class ChecklistValidators {
         }
         return { invalid: true };
     }
-
+    */
     static mandatoryFieldsDuplicationValidator(c: FormControl): ValidationErrors | null {
         if (c instanceof FormArray) {
             let isDuplicatesFound = false;
-            const userRoleForms = <FormArray>c;
+            const listItems = <FormArray>c;
             let ixRef = 0;
             let rowIx = 0;
-            for (ixRef; ixRef < userRoleForms.length; ixRef++) {
-                const formRef = userRoleForms.at(ixRef);
+            for (ixRef; ixRef < listItems.length; ixRef++) {
+                const formRef = listItems.at(ixRef);
                 let ix = 0;
-                for (ix; ix < userRoleForms.length; ix++) {
-                    const form = userRoleForms.at(ix);
+                for (ix; ix < listItems.length; ix++) {
+                    const form = listItems.at(ix);
                     // is the mandatory fiekds of forms are equals?
                     const isRoleTypeEquals = formRef.get('id')?.value === form.get('id')?.value;
                     const isModuleTypeEquals = formRef.get('label')?.value === form.get('label')?.value;
@@ -44,6 +44,29 @@ export class ChecklistValidators {
             }
             // return { duplication: isDuplicatesFound };
             return { 'duplication': { value : '(' + (ixRef + 1) + '. row vs. ' + (rowIx + 1) + '. row)'} };
+        }
+        return null;
+    }
+
+    /**
+     * 
+     * @param c: FormControl 
+     * @returns ValidationErrors 
+     */
+    static oneItemCheckRequiredValidator(c: FormControl): ValidationErrors | null {
+        if (c instanceof FormArray) {
+            let isOneItemRequiredError = false;
+            const listItems = <FormArray>c;
+            let ix = 0;
+            for (ix; ix < listItems.length; ix++) {
+                const formRef = listItems.at(ix);
+                let selectedFormControl = formRef.get('selected');
+                if (!selectedFormControl?.dirty || (selectedFormControl?.dirty && !selectedFormControl.value)) {
+                    isOneItemRequiredError = true;
+                    break;
+                }
+            }
+            return { 'oneItemRequired':  isOneItemRequiredError };
         }
         return null;
     }
