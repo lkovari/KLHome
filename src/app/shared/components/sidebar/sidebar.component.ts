6de4f722c8bcc216fd33eba064fd9c298923d236
LK <laszlo.kovary@gmail.com>
2022-11-20 22:34:07 +0100

    added pics to cv.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('11/20/2022 11:35 AM');
-  lastUpdateTooltip = 'updated cv.';
+  lastUpdate = new Date('11/20/2022 10:33 PM');
+  lastUpdateTooltip = 'added pics to cv.';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
