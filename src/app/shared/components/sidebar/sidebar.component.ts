388ab5f01f6599fddb415482e9b4937ef39a9b7c
LK <laszlo.kovary@gmail.com>
2022-11-23 22:02:05 +0100

    added icagile certificate

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('11/21/2022 10:13 PM');
-  lastUpdateTooltip = 'added awards to cv.';
+  lastUpdate = new Date('11/21/2022 10:01 PM');
+  lastUpdateTooltip = 'added icagile certificate.';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
