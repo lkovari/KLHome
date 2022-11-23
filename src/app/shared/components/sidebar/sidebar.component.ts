b59f409db6b4331e050e960df2c1c3eab498b5d0
LK <laszlo.kovary@gmail.com>
2022-11-23 22:36:32 +0100

    sidebar mess

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('11/21/2022 10:01 PM');
+  lastUpdate = new Date('11/21/2022 10:55 PM');
   lastUpdateTooltip = 'added icagile certificate.';
 
   ngOnInit() {
