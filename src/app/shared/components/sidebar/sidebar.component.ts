878d95f59537811472d4769f0c7aaaeae89fe343
LK <laszlo.kovary@gmail.com>
2022-12-04 19:44:29 +0100

    last updatze

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('12/04/2022 07:14 PM');
+  lastUpdate = new Date('12/04/2022 07:30 PM');
   lastUpdateTooltip = 'updated award images';
 
   ngOnInit() {
