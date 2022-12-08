f9726d1768765300610fb8dd80a661562651c197
LK <laszlo.kovary@gmail.com>
2022-12-08 13:39:43 +0100

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('12/08/2022 13:02 PM');
+  lastUpdate = new Date('12/08/2022 01:37 PM');
   lastUpdateTooltip = 'Factory, AbstractFactory details';
 
   ngOnInit() {
