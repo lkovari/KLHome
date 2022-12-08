ac9e81eb75e71bf5566c54f68e74682bcd93ae25
LK <laszlo.kovary@gmail.com>
2022-12-08 17:46:51 +0100

    Updates

--- app/layout/angular-page/angular-page-content16/builder-pattern/builder-pattern.component.ts
+++ app/layout/angular-page/angular-page-content16/builder-pattern/builder-pattern.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TextUtils } from '../text-utils';
 
 @Component({
   selector: 'app-builder-pattern',
@@ -6,10 +7,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./builder-pattern.component.scss']
 })
 export class BuilderPatternComponent implements OnInit {
-
+  pattern: string;
+  numOfTextRows: number | undefined;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.pattern = `
+    // Builder Concept Sample Code
+    class Product {
+        parts: string[] = [];
+    }
+    
+    interface IBuilder {
+        buildPartA(): this
+        buildPartB(): this
+        buildPartC(): this
+        getResult(): Product
+    }
+    
+    class Builder implements IBuilder {
+        // The Concrete Builder
+        product: Product;
+    
+        constructor() {
+          this.product = new Product();
+        }
+    
+        buildPartA() {
+          this.product.parts.push('a');
+          return this;
+        }
+    
+        buildPartB() {
+          this.product.parts.push('b');
+          return this;
+        }
+    
+        buildPartC() {
+          this.product.parts.push('c');
+          return this;
+        }
+    
+        getResult() {
+          return this.product;
+        }
+    }
+    
+    class Director {
+        // The Director, building a complex representation
+    
+        static construct() {
+          'Constructs and returns the final product'
+          return new Builder().buildPartA().buildPartB().buildPartC().getResult();
+        }
+    }
+    // The Client
+    const PRODUCT1 = Director.construct();
+    `;    
+    this.numOfTextRows = TextUtils.countTextRow(this.pattern);    
   }
 
 }
