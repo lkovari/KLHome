4f92d8e9e80176d60d3dd94d404c74b7a6ec918f
LK <laszlo.kovary@gmail.com>
2023-06-05 23:21:54 +0200

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('06/3/2023 07:17 AM');
-  lastUpdateTooltip = 'Checklist component modification';
+  lastUpdate = new Date('06/5/2023 00:137 PM');
+  lastUpdateTooltip = 'Fixed the Checklist component validation';
 
   ngOnInit() {
     this.lastUpdateTooltip = this.lastUpdateTooltip + '';
