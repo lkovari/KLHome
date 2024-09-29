8edf2fa0ceb80dd495a964ff227d005512125dde
lkovari <laszlo.kovary@gmail.com>
2024-09-29 02:18:29 +0200

    tst

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('06/9/2024 6:47 AM');
+  lastUpdate = new Date('06/9/2024 6:48 AM');
   lastUpdateTooltip = 'About page style, added vertical scroll';
 
   ngOnInit() {
