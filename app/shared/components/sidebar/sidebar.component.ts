adb8809f9b54875fac2afa7d11fc7343c3542c3d
lkovari <laszlo.kovary@gmail.com>
2025-04-08 20:05:35 +0200

    Updates

--- app/shared/components/sidebar/sidebar.component.ts
+++ app/shared/components/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   isActive = false;
   showMenu = '';
-  lastUpdate = new Date('04/8/5024 7:51 PM');
+  lastUpdate = new Date('04/8/2025 7:51 PM');
   lastUpdateTooltip = 'Added built-in sensors private policy';
 
   ngOnInit() {
